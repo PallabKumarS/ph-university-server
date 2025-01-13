9243099c5c3a0577a9557ca7928ee279ac9c1486
PallabKumarS <pallabkumar26@gmail.com>
2025-01-13 22:32:40 +0600

    added limited access for admin

--- src/app/modules/academicDepartment/academicDepartment.route.ts
+++ src/app/modules/academicDepartment/academicDepartment.route.ts
@@ -2,11 +2,14 @@ import express from 'express';
 import validateRequest from '../../middlewares/validateRequest';
 import { AcademicDepartmentValidation } from './academicDepartment.validation';
 import { AcademicDepartmentControllers } from './academicDepartment.controller';
+import auth from '../../middlewares/auth';
+import { USER_ROLE } from '../user/user.constant';
 
 const router = express.Router();
 
 router.post(
   '/create-academic-department',
+  auth(USER_ROLE.admin),
   validateRequest(
     AcademicDepartmentValidation.createAcademicDepartmentValidationSchema,
   ),
@@ -15,17 +18,23 @@ router.post(
 
 router.get(
   '/:departmentId',
+  auth(USER_ROLE.admin),
   AcademicDepartmentControllers.getSingleAcademicDepartment,
 );
 
 router.patch(
   '/:departmentId',
+  auth(USER_ROLE.admin),
   validateRequest(
     AcademicDepartmentValidation.updateAcademicDepartmentValidationSchema,
   ),
   AcademicDepartmentControllers.updateAcademicDepartment,
 );
 
-router.get('/', AcademicDepartmentControllers.getAllAcademicDepartments);
+router.get(
+  '/',
+  auth(USER_ROLE.admin),
+  AcademicDepartmentControllers.getAllAcademicDepartments,
+);
 
 export const AcademicDepartmentRoutes = router;
